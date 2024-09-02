3db69a8d427fea7ce52dc95c24dd0b09c96d586e
giorgi <bidzishviligiorgi7@gmail.com>
2024-09-02 11:49:23 +0400

    recipe details fetching added

--- src/app/views/details-pg/details-pg.component.ts
+++ src/app/views/details-pg/details-pg.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
+import { ActivatedRoute } from '@angular/router';
+import { takeUntil, switchMap, of } from 'rxjs';
+import { Unsubscriber } from '../../services/unsubscriber/unsubscriber.service';
+import { RecipesStore } from '../../store/recipes.store';
 @Component({
   selector: 'app-details-pg',
   standalone: true,
   imports: [MatListModule],
   templateUrl: './details-pg.component.html',
 })
-export class DetailsPgComponent {}
+export class DetailsPgComponent extends Unsubscriber implements OnInit {
+  private route = inject(ActivatedRoute);
+  private store = inject(RecipesStore);
+  ngOnInit(): void {
+    this.route.queryParams
+      .pipe(
+        takeUntil(this.destroy$),
+        switchMap((params: any) =>
+          params.id ? this.store.getRecipeById(params.id) : of(null)
+        )
+      )
+      .subscribe(console.log);
+  }
+}

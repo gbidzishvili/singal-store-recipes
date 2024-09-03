b3b9cc9c1e870071649c8f50ed22ebfd1bf635f5
giorgi <bidzishviligiorgi7@gmail.com>
2024-09-03 19:40:17 +0400

    views/add-recipe-pg

--- src/app/views/add-recipe-pg/add-recipe-pg.component.ts
+++ src/app/views/add-recipe-pg/add-recipe-pg.component.ts
@@ -13,10 +13,13 @@ import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { RecipesStore } from '../../store/recipes.store';
-import { takeUntil } from 'rxjs';
+import { catchError, map, of, takeUntil, tap } from 'rxjs';
 import { Unsubscriber } from '../../services/unsubscriber/unsubscriber.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../shared/dialogs/confirm-dialog/confirm-dialog.component';
 @Component({
   selector: 'app-add-recipe-pg',
   standalone: true,
@@ -30,17 +33,20 @@ import { MatIconModule } from '@angular/material/icon';
     MatCheckboxModule,
     MatButtonModule,
     MatIconModule,
+    RouterModule,
+    MatDialogModule,
   ],
   templateUrl: './add-recipe-pg.component.html',
 })
 export class AddRecipePgComponent extends Unsubscriber implements OnInit {
+  isSaved = false;
   store = inject(RecipesStore);
   fb = inject(FormBuilder);
+  readonly dialog = inject(MatDialog);
   addRecipeForm!: FormGroup;
   ngOnInit(): void {
     this.initForm();
   }
-
   initForm() {
     this.addRecipeForm = this.fb.group({
       id: [],
@@ -67,7 +73,6 @@ export class AddRecipePgComponent extends Unsubscriber implements OnInit {
   onFileUpload(event: Event) {
     const fileInputElement = event.target as HTMLInputElement;
     if (fileInputElement.files && fileInputElement.files[0]) {
-      console.log(fileInputElement.files[0]);
       let reader = new FileReader();
       reader.onloadend = () => {
         let baseStringResult = reader.result as string;
@@ -76,12 +81,30 @@ export class AddRecipePgComponent extends Unsubscriber implements OnInit {
       reader.readAsDataURL(fileInputElement.files[0]);
     }
   }
+  canDeactivate(): any {
+    if (this.addRecipeForm.dirty) {
+      const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+        width: '250px',
+        data: {
+          title: 'usaved Changes',
+          content: 'Changes will not be saved would you like to proceed',
+        },
+      });
+      return dialogRef.afterClosed().pipe(map((result) => !!result));
+    }
+    return true;
+  }
   onSubmit() {
     this.store
       .addRecipe(this.addRecipeForm.value)
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((recipe) =>
-        console.log('subscription in add recipe:', recipe)
-      );
+      .pipe(
+        takeUntil(this.destroy$),
+        tap(() => alert('recipe added')),
+        catchError((_) => {
+          alert('error has happened');
+          return of([]);
+        })
+      )
+      .subscribe();
   }
 }

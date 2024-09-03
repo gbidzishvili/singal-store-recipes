14284d6e5c1199afdf33cd0124521b507b7cd18d
giorgi <bidzishviligiorgi7@gmail.com>
2024-09-03 16:19:08 +0400

    mat-dialog added

---
+++ src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule],
+  templateUrl: './confirm-dialog.component.html',
+})
+export class ConfirmDialogComponent {
+  data = inject(MAT_DIALOG_DATA);
+}

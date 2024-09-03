2d4f254b08bf26a434ce2e7f3656f8f2e4312ec1
giorgi <bidzishviligiorgi7@gmail.com>
2024-09-03 19:35:44 +0400

    header added inside app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecipesStore } from './store/recipes.store';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
   store = inject(RecipesStore);
-  ngOnInit(): void {
-    // this.store.loadAll().subscribe((v) => console.log('ereek+leqso'));
-  }
+  ngOnInit(): void {}
   title = 'signal-store-recipes';
 }

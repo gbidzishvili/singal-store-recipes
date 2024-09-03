9f921ed950e0529291220673cec0da4b89493677
giorgi <bidzishviligiorgi7@gmail.com>
2024-09-03 13:41:21 +0400

    filtration added to home-pg

--- src/app/views/home-pg/home-pg.component.ts
+++ src/app/views/home-pg/home-pg.component.ts
@@ -11,7 +11,7 @@ import { MatSelect, MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { RecipeCardComponent } from '../../shared/components/recipe-card/recipe-card.component';
 import { Unsubscriber } from '../../services/unsubscriber/unsubscriber.service';
-import { takeUntil } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -37,11 +37,20 @@ export class HomePgComponent extends Unsubscriber {
   ngOnInit() {
     this.loadRecipes().pipe(takeUntil(this.destroy$)).subscribe(console.log);
   }
-  onChangeValue(event: any) {
-    console.log(event);
-  }
   loadRecipes() {
     return this.store.loadAll();
+  }
+  onChangeFavorites(value: string) {
+    // console.log(event.value, event, 'asdfasdlika');
+    const filter = value === 'All' ? false : true;
+    this.store.updateFilter({ favorites: filter });
+  }
+  onChangeCategory(value: string) {
+    this.store.updateFilter({ category: value });
+  }
+  onInputChange(value: string) {
+    console.log(value);
+    this.store.updateFilter({ name: value });
   }
   handleDeleteClick(id: string) {
     if (confirm('Do you want to delete this Recipe'))
@@ -50,12 +59,18 @@ export class HomePgComponent extends Unsubscriber {
         .pipe(takeUntil(this.destroy$))
         .subscribe((v) => console.log('delete recipe in home-pg', v));
   }
-  // handleEditClick(id: string) {
-  //   this.router.navigate(['/edit'], { queryParams: { id: id } });
-  // }
-  // showDetails(id: string) {
-  //   this.router.navigate(['/details'], { queryParams: { id: id } });
-  // }
+  handleFavoriteClick(event: any) {
+    // best practice needed change store without reloading view
+    console.log(event);
+    this.store
+      .updateRecipe(event.id, { favorite: event.favorite })
+      .pipe(
+        takeUntil(this.destroy$),
+        switchMap(() => this.store.loadAll())
+      )
+      .subscribe(console.log);
+    console.log('recipes', this.store.recipes());
+  }
   goTo(id: string, endpoint: string) {
     this.router.navigate([endpoint], { queryParams: { id: id } });
   }
